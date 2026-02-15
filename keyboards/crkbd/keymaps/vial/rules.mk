b2bf3a9bee09dc2322ff4fcd3461e24c36faef32
rizalfr <zacksupreme@gmail.com>
2026-02-15 16:02:24 +0700

    mb logo and vial

---
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,18 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+VIAL_INSECURE       = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = no
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
+
+CAPS_WORD_ENABLE = no
+LAYER_LOCK_ENABLE = no
+REPEAT_KEY_ENABLE = no
